cab4f0512d4bda7733d6820d2d5f767aadd13cf6
markj82 <markjohn666@gmail.com>
2019-06-25 19:39:50 +0100

    add articleRouter

--- routers/api-router.js
+++ routers/api-router.js
@@ -3,8 +3,10 @@ const apiRouter = express();
 
 const topicRouter = require('./topic-router')
 const userRouter = require('./user-router')
+const articleRouter = require('./article-router')
 
 apiRouter.use('/topics', topicRouter)
 apiRouter.use('/users', userRouter)
+apiRouter.use('/articles', articleRouter)
 
 module.exports = apiRouter
