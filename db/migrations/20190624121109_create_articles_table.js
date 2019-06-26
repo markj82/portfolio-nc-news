e9b2c789574199072dba83cf7d4f6f7d12d42ab5
markj82 <markjohn666@gmail.com>
2019-06-26 12:53:26 +0100

    add default value to votes

--- db/migrations/20190624121109_create_articles_table.js
+++ db/migrations/20190624121109_create_articles_table.js
@@ -4,7 +4,7 @@ exports.up = function(connection, Promise) {
         articlesTable.increments('article_id').primary().notNullable();
         articlesTable.string('title').notNullable();
         articlesTable.text('body').notNullable();
-        articlesTable.integer('votes'); // set default to 0 later
+        articlesTable.integer('votes').defaultTo(0);
         articlesTable.string('topic').references('topics.slug');
         articlesTable.string('author').references('users.username').notNullable();
         articlesTable.string('created_at') // timestamp
