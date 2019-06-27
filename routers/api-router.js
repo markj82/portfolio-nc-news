d9099c89fbe986ef16313d276bb6d433544cc22f
markj82 <markjohn666@gmail.com>
2019-06-27 15:44:18 +0100

    add commentRouter

--- routers/api-router.js
+++ routers/api-router.js
@@ -4,9 +4,11 @@ const apiRouter = express();
 const topicRouter = require('./topic-router')
 const userRouter = require('./user-router')
 const articleRouter = require('./article-router')
+const commentRouter = require('./comment-router')
 
 apiRouter.use('/topics', topicRouter)
 apiRouter.use('/users', userRouter)
 apiRouter.use('/articles', articleRouter)
+apiRouter.use('/comments', commentRouter)
 
 module.exports = apiRouter
