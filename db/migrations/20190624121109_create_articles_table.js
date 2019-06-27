3c1b7408e0e03daac37cf7f18e7579789e67b45a
markj82 <markjohn666@gmail.com>
2019-06-27 09:38:27 +0100

    add timestamp

--- db/migrations/20190624121109_create_articles_table.js
+++ db/migrations/20190624121109_create_articles_table.js
@@ -7,7 +7,7 @@ exports.up = function(connection, Promise) {
         articlesTable.integer('votes').defaultTo(0);
         articlesTable.string('topic').references('topics.slug');
         articlesTable.string('author').references('users.username').notNullable();
-        articlesTable.string('created_at') // timestamp
+        articlesTable.timestamp('created_at').defaultTo(connection.fn.now());
   
     })
   };
