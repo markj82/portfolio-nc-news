35a4969259822a956ba834cdce52ccb5bdf4424e
markj82 <markjohn666@gmail.com>
2019-06-27 09:37:40 +0100

    add notNullable

--- db/migrations/20190624121120_create_comments_table.js
+++ db/migrations/20190624121120_create_comments_table.js
@@ -2,11 +2,11 @@
 exports.up = function(connection, Promise) {
   return connection.schema.createTable('comments', function(commentsTable) {
       commentsTable.increments('comment_id').primary();
-      commentsTable.string('author').references('users.username');
-      commentsTable.integer('article_id').references('articles.article_id');
+      commentsTable.string('author').references('users.username').notNullable();
+      commentsTable.integer('article_id').references('articles.article_id').notNullable();
       commentsTable.integer('votes').defaultTo(0);
       commentsTable.timestamp('created_at').defaultTo(connection.fn.now());;
-      commentsTable.text('body')
+      commentsTable.text('body').notNullable()
   })
 };
 
