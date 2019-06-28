ea4b1e0521c6446443e348abecb20dde947a8d4d
markj82 <markjohn666@gmail.com>
2019-06-28 10:35:20 +0100

    add catch(next)

--- controllers/topic-controllers.js
+++ controllers/topic-controllers.js
@@ -3,5 +3,5 @@ const { fetchTopics } = require('../models/topic-model');
 exports.sendTopics = (req, res, next) => {
     fetchTopics().then(topics => {
         res.status(200).send({topics})
-    })
+    }).catch(next)
 }
