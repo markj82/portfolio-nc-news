e5c163e8338a9dbaa910e3892e8112bd3f53d822
markj82 <markjohn666@gmail.com>
2019-06-28 13:35:33 +0100

    add route all with handleMethodErrors

--- routers/api-router.js
+++ routers/api-router.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const apiRouter = express();
+const { handleMethodErrors} = require('../errors/index')
 
 const topicRouter = require('./topic-router')
 const userRouter = require('./user-router')
 const articleRouter = require('./article-router')
 const commentRouter = require('./comment-router')
+
+apiRouter.route('/')
+    .all(handleMethodErrors)
 
 apiRouter.use('/topics', topicRouter)
 apiRouter.use('/users', userRouter)
