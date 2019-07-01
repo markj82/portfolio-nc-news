bc0b8c493cb2193c725155f3acc6b6cd065c203f
markj82 <markjohn666@gmail.com>
2019-07-01 11:22:50 +0100

    remove comments

--- errors/index.js
+++ errors/index.js
@@ -1,12 +1,9 @@
-
 exports.handleCustomErrors = (err, req, res, next) => {
-    // console.log(err, '<< err from index.js errors')
     if (err.status) res.status(err.status).send({ msg: err.msg });
     else next(err);
 };
   
 exports.handlePsqlErrors = (err, req, res, next) => {
-    // console.log(err, '<< err from index.js errors')
     const sqlErrorCodes = {
         '22P02': 'Invalid id',
         '23502': 'Missing Required Information',
@@ -19,17 +16,13 @@ exports.handlePsqlErrors = (err, req, res, next) => {
 }; 
 
 exports.routeNotFoundError = (req, res, next) => {
-    // console.log(err, '<< err from index.js errors')
     res.status(404).send({ msg: 'Not Found!' })
 }
 
 exports.handleMethodErrors = (req, res, next) => {
-    // console.log(err, '<< err from index.js errors')
     res.status(405).send({msg: 'Method Not Allowed'})
 }
 
 exports.handleServerErrors = (err, req, res, next) => {
-
-    // console.log(err, '<< err from index.js errors')
     res.status(500).send({ msg: 'Internal Server Error' });
 };
